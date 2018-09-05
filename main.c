8e80f3eb1ab8645f52968aaa31418e85252b8746
forty <18188116781@163.com>
2018-09-05 21:52:00 +0800

    understand passwd

--- main.c
+++ main.c
@@ -13,8 +13,8 @@
 #define maxn_dirname 1024
 #define maxn_hostname 256
 
-char* current_dir;
-char* hostname;
+char current_dir[maxn_dirname];
+char hostname[maxn_hostname];
 struct passwd* pass_wd;
 int display_prompt()
 {
@@ -28,13 +28,12 @@ int main(int argc, char* argv[])
 		assert(argv[i]); // specification
 		printf("argv[%d] = %s\n", i, argv[i]);
  		} 
-	current_dir = malloc(maxn_dirname);
-	getcwd(current_dir, sizeof(current_dir));
-	hostname = malloc(maxn_hostname);
+	//getcwd(current_dir, sizeof(current_dir));//初始化认为在~中,当用到cd的时候再更换看当前目录
 	pass_wd = malloc(sizeof(struct passwd));
 	pass_wd = getpwuid(getuid());
+	strncpy(current_dir, pass_wd->pw_dir, sizeof(current_dir));
 
 	//printf("pw_name:%s\npw_passwd:%s\npw_gecos:%s\npw_dir:%s\npw_shell:%s\n", pass_wd->pw_name, pass_wd->pw_passwd, pass_wd->pw_gecos, pass_wd->pw_dir, pass_wd->pw_shell);
-		
+	printf("current_dir:%s\n", current_dir);	
 	return 0;
 }
