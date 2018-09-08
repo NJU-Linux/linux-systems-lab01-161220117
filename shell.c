7017d44996cda6383cb2d4a3506dc6f1edc0736c
forty <18188116781@163.com>
2018-09-08 14:26:38 +0800

    make

--- shell.c
+++ shell.c
@@ -290,8 +290,6 @@ void do_command()
 				in_fd = open(p_cmd->in_file, O_RDONLY, 0666);
 				dup2(in_fd, STDIN_FILENO);
 			}
-			printf("hahahahaha\n");
-			printf("before execvp: para1: %s %s", p_cmd->para1[p_cmd->para1_cnt-1], p_cmd->para1[p_cmd->para1_cnt-1]);
 			execvp(p_cmd->command1, p_cmd->para1);
 		}
 		/*父进程*/
@@ -302,6 +300,7 @@ void do_command()
 				if(pid2 == 0){
 					close(pipefd[1]);
 					dup2(pipefd[0], STDIN_FILENO);
+					printf("before execvp: para1: %s %s", p_cmd->para1[p_cmd->para1_cnt-1], p_cmd->para1[p_cmd->para1_cnt-1]);
 					execvp(p_cmd->command2, p_cmd->para2);
 				}
 				else{
