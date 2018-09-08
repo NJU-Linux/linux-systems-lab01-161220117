befc2e9467b9e4ccb4fb70368f5f6793711eda3d
forty <18188116781@163.com>
2018-09-09 02:37:04 +0800

    make

--- shell.c
+++ shell.c
@@ -326,8 +326,8 @@ void history_command()
 		else{
 			char* search_entity = malloc(strlen(p_cmd->command1));
 			strcpy(search_entity, p_cmd->command1+1);
-			printf("search_entity:%s\n", search_entity);
 			HIST_ENTRY* entry = histr[0];
+			printf("entry:%s\n", entry->line);
 			int i = 1;
 			while(histr[i]!=NULL){
 				entry = histr[i++];
