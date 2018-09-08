2878f7408dbee94c38e18e8ca40f344bab1dc614
forty <18188116781@163.com>
2018-09-08 09:26:07 +0800

    f

--- testcd.c
+++ testcd.c
@@ -4,15 +4,20 @@
 
 int main()
 {
-	char *s = "/home/tsm";
+	char *s = "~";
 	char *pwd = malloc(64);
 	if(!getcwd(pwd,64)){
-		perror("getcwd error");
+		perror("getcwd error\n");
 		return 1;
 	}
 	int ret = chdir(s);
 	if(ret){
 		printf("chdir error\n");
 	}
+	if(!getcwd(pwd, 64)){
+		perror("getcwd error\n");
+		return 1;
+	}
+	printf("pwd:%s\n", pwd);
 	return 0;
 }
